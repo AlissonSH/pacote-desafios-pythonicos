632c86245ccf4873deec35a5a5574218d27d492d
AlissonSH <86666129+AlissonSH@users.noreply.github.com>
2023-01-05 00:30:02 -0300

    Update 00_fizzbuzz.py

--- 00_fizzbuzz.py
+++ 00_fizzbuzz.py
@@ -10,34 +10,33 @@ Regras do FizzBuzz!
 import pytest
 
 def jogo(pos):
-  exit = str(pos)
-  if pos % 3 == 0 and pos % 5 == 0:
-    exit = 'fizzbuzz'
-  elif pos % 3 == 0:
-    exit = 'fizz'
-  elif pos % 5 == 0:
-    exit = 'buzz'
-  return exit
+    exit = str(pos)
+    if pos % 3 == 0 and pos % 5 == 0:
+        exit = 'fizzbuzz'
+    elif pos % 3 == 0:
+        exit = 'fizz'
+    elif pos % 5 == 0:
+        exit = 'buzz'
+    return exit
 
 
 def test_jogo():
-  assert jogo(1) == '1'
-  assert jogo(2) == '2'
-  assert jogo(4) == '4'
-  
-  assert jogo(3) == 'fizz'
-  assert jogo(6) == 'fizz'
-  assert jogo(9) == 'fizz'
+    assert jogo(1) == '1'
+    assert jogo(2) == '2'
+    assert jogo(4) == '4'
+    
+    assert jogo(3) == 'fizz'
+    assert jogo(6) == 'fizz'
+    assert jogo(9) == 'fizz'
+    
+    assert jogo(5) == 'buzz'
+    assert jogo(10) == 'buzz'
+    assert jogo(20) == 'buzz'
 
-  assert jogo(5) == 'buzz'
-  assert jogo(10) == 'buzz'
-  assert jogo(20) == 'buzz'
-
-  assert jogo(15) == 'fizzbuzz'
-  assert jogo(30) == 'fizzbuzz'
-  assert jogo(45) == 'fizzbuzz'
+    assert jogo(15) == 'fizzbuzz'
+    assert jogo(30) == 'fizzbuzz'
+    assert jogo(45) == 'fizzbuzz'
 
 
 if __name__ == "__main__":
-  pytest.main(['-svv', __file__])
-  
+    pytest.main(['-svv', __file__])
